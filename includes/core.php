afeb93a149158ab12849308e0b95ddab5bd44df3
Fernando Mendez <fernandomendez101@gmail.com>
2015-03-06 21:25:35 -0500

    Updating Core and Index OOP

--- includes/core.php
+++ includes/core.php
@@ -4,19 +4,33 @@
  *  Provides functionality for everything that is needed in the application
  *
  * 
- *  Operations Control Room Mobile App
+ *  Operations Control Room App
  *  Miami - Smart Cities Hackathon @ FIU 03/06/15 - 03/08/15
  */
 
 
 
+$core = new Core();
+
+
 class Core {
-    
+    private $db;
+
+
+    function __construct(){
+        if(!defined("DEBUG")){
+            define("DEBUG", true);
+        }
+
+
+
+
+    }
 
     public static function writeDebug($str){
-
-        echo "<b>DEBUG MESSAGE:  </b>" . $str;
-
+        if(DEBUG) {
+            echo "<b>DEBUG MESSAGE:  </b>" . $str;
+        }
 
     }
 
--- index.php
+++ index.php
@@ -4,14 +4,13 @@
  *  Provides login to panel with no other functionality until logged in
  *  When logged in provides access to the interface to report.
  *
- *  Operations Control Room Mobile App
+ *  Operations Control Room App
  *  Miami - Smart Cities Hackathon @ FIU 03/06/15 - 03/08/15
  */
 
-
+#define
 include_once 'includes/core.php';
 
-Core::writeDebug("Initialized");
-
+$core->writeDebug("Test");
 
 ?>
