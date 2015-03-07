10627b0bd2ecefd390da6d86a6fcb17f9008e558
Fernando Mendez <fernandomendez101@gmail.com>
2015-03-06 21:14:06 -0500

    Initial Push
    Initial push, starting structuring

---
+++ includes/core.php
@@ -0,0 +1,25 @@
+<?php
+/*  Core - Core file included by all other scripts
+ *
+ *  Provides functionality for everything that is needed in the application
+ *
+ * 
+ *  Operations Control Room Mobile App
+ *  Miami - Smart Cities Hackathon @ FIU 03/06/15 - 03/08/15
+ */
+
+
+
+class Core {
+    
+
+    public static function writeDebug($str){
+
+        echo "<b>DEBUG MESSAGE:  </b>" . $str;
+
+
+    }
+
+}
+
+
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/*  Index.php - Main View
+ *
+ *  Provides login to panel with no other functionality until logged in
+ *  When logged in provides access to the interface to report.
+ *
+ *  Operations Control Room Mobile App
+ *  Miami - Smart Cities Hackathon @ FIU 03/06/15 - 03/08/15
+ */
+
+
+include_once 'includes/core.php';
+
+Core::writeDebug("Initialized");
+
+
+?>
